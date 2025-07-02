bc2c0a6a8cddf5aeb20aeb4193a54a3c3a236566
Saumya Pandey <saumyap271@gmail.com>
2025-07-02 11:00:33 +0000

    container config done

---
+++ 01_Basic/02_variables.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all Js code as newer version
+//alert(3+3) //we are using nodejs,not browser
+//datatypes
+
+//number=> 2 to the power of 53
+//BigInt
+//string=>""
+//boolean=>true/false
+//null=>empty value
+//undefined=>value assign ni huii abhi
+//symbol=>unique value
+//object=>
+ console.log(typeof null);    
+//typeof null is objects
+//typeof undefined is undefined
