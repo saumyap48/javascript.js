6fbe47f1a7c12f4ce49ee5c632c85079d4f523da
Saumya Pandey <saumyap271@gmail.com>
2025-07-02 10:02:58 +0000

    container config done

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553;
+let accountEmail="saumyap100@gmail.com";
+var accountPassword="12345";
+accountCity="jaipur";
+let accountState; //not throw an error give output as undefined
+
+//accountId=2;
+//Not allowed reassgiment and redecleration of const variable
+/*
+prefer not to use var
+because of issue of scope variable and function scope
+*/
+console.log(accountId);
+console.log([accountId,accountEmail,accountPassword,accountCity]);
