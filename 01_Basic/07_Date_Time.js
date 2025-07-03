0ef8c816fcc898fb6c8bc8fa9d77e9ad6ebcd17d
Saumya Pandey <saumyap271@gmail.com>
2025-07-03 13:54:16 +0000

    container config done

---
+++ 01_Basic/07_Date_Time.js
@@ -0,0 +1,24 @@
+//date is an object
+let myDate=new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate= new Date(2023,0,24)
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
+const myCreatedDatee=new Date("01-14-2023");
+console.log(myCreatedDatee.toLocaleString)
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDatee.getTime());
+console.log(Math.floor(Date.now()/1000)); 
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay()); 
+newDate.toLocaleString('default',{
+    weekday:"long",
+
+})
