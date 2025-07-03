c2b8356ba0ee25a40f8f14c6a8cd54f8f4f5e18d
Saumya Pandey <saumyap271@gmail.com>
2025-07-03 16:28:30 +0000

    container config done

---
+++ 02_Basic/01_arrays.js
@@ -0,0 +1,14 @@
+const myArr=[1,2,3,4,5]; 
+/* console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+const newarr=myArr.join();
+console.log(myArr);
+console.log(newarr);
+console.log(typeof newarr); */
+console.log("A",myArr);
+const myn1=myArr.slice(1,3);
+console.log(myn1);
+console.log("B",myArr);
+const myn2=myArr.splice(1,3);
+console.log("C",myArr);
+console.log(myn2);
